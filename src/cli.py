87926d5687bd30d4f0d924e356744df48db89416
Emir <amanbekoff@gmail.com>
2018-05-28 13:45:54 +0600

    Refactor code

--- src/cli.py
+++ src/cli.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.6
 import argparse
 import urllib.request
 import urllib.error
@@ -14,16 +14,13 @@ def main():
     parser.add_argument('--password', '-p', type=str, default='password',
                         help='Your bitbucket account password. For private repositories only')
     args = parser.parse_args()
-    print('Username: %s\n'
-          'Repository: %s\n'
-          'Password: %s\n' % (args.username, args.repository, args.password))
+    # Handle some errors
     try:
         id_list = get_id_list(args.username, args.repository)
         pr_list = [
             get_pr_by_id(args.username, args.repository, pr_id)
             for pr_id in id_list
         ]
-        print('Filtering assigned to you PRs')
         pr_list = list(filter(lambda pr: any(
             part['role'] == 'REVIEWER' and
             not part['approved'] and
@@ -33,9 +30,12 @@ def main():
         beauty_print(pr_list)
     except urllib.error.HTTPError:
         print('Wrong username or repository!')
+    except urllib.error.URLError:
+        print('No internet connection!')
 
 
 def beauty_print(pr_list):
+    """Function for getting formatted print"""
     for pr in pr_list:
         print('_' * 20)
         print('TITLE: %s' % pr['title'])
@@ -44,8 +44,7 @@ def beauty_print(pr_list):
 
 
 def get_id_list(username, repository):
-    print('Getting all pull requests...')
-
+    """Gets all pull request IDs"""
     url = 'https://api.bitbucket.org/2.0/' \
           'repositories/%s/%s/pullrequests/?state=OPEN' % (username, repository)
     data = get_request(url)
@@ -55,15 +54,17 @@ def get_id_list(username, repository):
 
 
 def get_pr_by_id(username, repository, pr_id):
+    """Gets detailed info about pull request"""
     url = 'https://api.bitbucket.org/2.0/' \
           'repositories/%s/%s/pullrequests/%d' % (username, repository, pr_id)
     data = get_request(url)
-    keys = ['title', 'description', 'created_on', 'participants', 'links']
+    keys = ['title', 'description', 'participants', 'links']
     data = {key: data[key] for key in keys}
     return data
 
 
 def get_request(url):
+    """Function for making get requests"""
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
         data = json.load(response)
