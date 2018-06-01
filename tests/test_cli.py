4056a5e411e81a135dd56b7cc11f3d93ea6d634a
Emir <amanbekoff@gmail.com>
2018-06-01 16:38:47 +0600

    Add tests for beauty print function

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,10 +13,15 @@ class TestCli(unittest.TestCase):
             cli.get_id_list('DoesntExist', 'DoesntExist')
 
     def test_get_pr_by_id(self):
-        self.assertEqual(cli.get_pr_by_id('bookinman', 'test-repo', 3), mock_data.pr_by_id)
+        self.assertEqual(cli.get_pr_by_id('bookinman', 'test-repo', 3), mock_data.pr_by_id_response)
 
         with self.assertRaises(HTTPError):
             cli.get_pr_by_id('DoesntExist', 'DoesntExist', 0)
+
+    def test_beauty_print(self):
+        self.assertEqual(cli.beauty_print(mock_data.beauty_print_parameters), mock_data.beauty_print_response)
+
+        self.assertTrue(type(cli.beauty_print(mock_data.beauty_print_parameters)) is str)
 
 
 if __name__ == '__main__':
