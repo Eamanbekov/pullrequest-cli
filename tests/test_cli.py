9211db5ae2e6e248a7e3dd3ecb3dacd1787b0b3e
Emir <amanbekoff@gmail.com>
2018-06-03 14:57:28 +0600

    Update tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,16 +7,16 @@ from tests import mock_data
 class TestCli(unittest.TestCase):
 
     def test_get_id_list(self):
-        self.assertEqual(cli.get_id_list('bookinman', 'test-repo'), [3, 1])
+        self.assertEqual(cli.get_id_list('bookinman', 'test-repo', ''), [3, 1])
 
         with self.assertRaises(HTTPError):
-            cli.get_id_list('DoesntExist', 'DoesntExist')
+            cli.get_id_list('DoesntExist', 'DoesntExist', '')
 
     def test_get_pr_by_id(self):
-        self.assertEqual(cli.get_pr_by_id('bookinman', 'test-repo', 3), mock_data.pr_by_id_response)
+        self.assertEqual(cli.get_pr_by_id('bookinman', 'test-repo', 3, ''), mock_data.pr_by_id_response)
 
         with self.assertRaises(HTTPError):
-            cli.get_pr_by_id('DoesntExist', 'DoesntExist', 0)
+            cli.get_pr_by_id('DoesntExist', 'DoesntExist', 0, '')
 
     def test_beauty_print(self):
         self.assertEqual(cli.beauty_print(mock_data.beauty_print_parameters), mock_data.beauty_print_response)
