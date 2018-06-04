d4e17cb72bea96dabd6393fd2e42f52e8e380b7f
Emir <amanbekoff@gmail.com>
2018-06-05 01:07:49 +0600

    Add mock test for get_pr_list function

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,13 +1,23 @@
-import unittest
+from unittest import TestCase, main
+from unittest.mock import patch
 from urllib.error import HTTPError
 from src import cli
 from tests import mock_data
 
 
-class TestCli(unittest.TestCase):
+class TestCli(TestCase):
 
     def test_get_pr_list(self):
-        self.assertEqual(cli.get_pr_list('bookinman', 'test-repo', ''), mock_data.pr_list_response)
+        with patch('src.cli.get_request') as mocked_get:
+            mocked_get.return_value = mock_data.get_request_response
+            # Test without password
+            pr_list = cli.get_pr_list('bookinman', 'test-repo', '')
+            mocked_get.assert_called_with(mock_data.get_request_url)
+            self.assertEqual(pr_list, mock_data.pr_list_response)
+            # Test with password
+            pr_list = cli.get_pr_list('bookinman', 'test-repo', 'password')
+            mocked_get.assert_called_with(mock_data.get_request_url, mock_data.get_request_auth)
+            self.assertEqual(pr_list, mock_data.pr_list_response)
 
         with self.assertRaises(HTTPError):
             cli.get_pr_list('DoesntExist', 'DoesntExist', '')
@@ -19,4 +29,4 @@ class TestCli(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
