8d539e8f386d9dd52856db53d4e984e6be3d46ab
Emir <amanbekoff@gmail.com>
2018-06-04 23:58:24 +0600

    Add one more test for new function

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,5 @@
 import unittest
-from urllib.error import HTTPError, URLError
+from urllib.error import HTTPError
 from src import cli
 from tests import mock_data
 
@@ -8,6 +8,9 @@ class TestCli(unittest.TestCase):
 
     def test_get_pr_list(self):
         self.assertEqual(cli.get_pr_list('bookinman', 'test-repo', ''), mock_data.pr_list_response)
+
+        with self.assertRaises(HTTPError):
+            cli.get_pr_list('DoesntExist', 'DoesntExist', '')
 
     def test_beauty_print(self):
         self.assertEqual(cli.beauty_print(mock_data.beauty_print_parameters), mock_data.beauty_print_response)
